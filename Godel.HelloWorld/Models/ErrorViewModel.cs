c60777afe3e754502ea3ed3a1c84ef0c4c8b65a7
dzmitry.siamashka@dominos.co.uk <dzmitry.siamashka@dominos.co.uk>
2020-07-16 13:35:41 +0300

    add a setting to model

--- Godel.HelloWorld/Models/ErrorViewModel.cs
+++ Godel.HelloWorld/Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace Godel.HelloWorld.Models
 {
     public class ErrorViewModel
     {
+        public string Setting { get; set; }
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
